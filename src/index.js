ee9f552fc7e207683ce5a979f9567c7013f6d4bc
lizcory <evcory96@gmail.com>
2025-04-30 13:45:10 -0400

    testing, upgrading dependencies

--- src/index.js
+++ src/index.js
@@ -53,7 +53,8 @@ const settings = Object.assign(
   // }))
 );
 
-fetch('https://na-data-sheetsstorm.s3.us-west-2.amazonaws.com/prod/epp/varying_degrees_2024.json').then(response => response.json()).then((_data)=>{
+// fetch('https://na-data-sheetsstorm.s3.us-west-2.amazonaws.com/prod/epp/varying_degrees_2024.json').then(response => response.json()).then((_data)=>{
+fetch('/src/lib/data/varying_degrees_2024.json').then(response => response.json()).then((_data)=>{
   data = _data;
   comparison_demographic = data.meta[0].comparison_demographic;
   total_demographic = data.meta[0].demographic_key_for_total;
